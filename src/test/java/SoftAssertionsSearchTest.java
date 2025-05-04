19fd654bf9a2e5c7c5c49bee52b87f0afdc7c862
Aleksandr <alexander1996osinin@mail.ru>
2025-05-04 14:06:02 +0300

    First test pass

---
+++ src/test/java/SoftAssertionsSearchTest.java
@@ -0,0 +1,42 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SoftAssertionsSearchTest {
+
+    private String text = """
+            @ExtendWith({SoftAssertsExtension.class})
+            class Tests {
+              @Test
+              void test() {
+                Configuration.assertionMode = SOFT;
+                open("page.html");
+            
+                $("#first").should(visible).click();
+                $("#second").should(visible).click();
+              }
+            """;
+
+    @BeforeAll
+    static void setup() {
+        Configuration.browser = "chrome";
+        Configuration.browserSize = "1920x1080";
+        Configuration.browserCapabilities = new ChromeOptions().addArguments("--remote-allow-origins=*");
+    }
+
+
+    @Test
+    void findJUnit5Code() {
+        open("https://github.com/selenide/selenide");
+        $("#wiki-tab").click();
+        $(".js-wiki-more-pages-link").click();
+        $("a[href='/selenide/selenide/wiki/SoftAssertions']").shouldHave(text("Soft assertions")).click();
+        $$(".markdown-heading").findBy(text("3. Using JUnit5 extend test class:")).sibling(0).$("pre")
+                .shouldHave(text(text));
+    }
+
+}
